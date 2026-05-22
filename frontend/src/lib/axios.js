ea13634b3a54ecb58af720cad9ecc3a1b41713ab
ShashankGaneshPai <paishashank0123@gmail.com>
2026-05-22 19:59:10 +0530

    frontend production api setup

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,13 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+//   withCredentials: true,
+// });
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
